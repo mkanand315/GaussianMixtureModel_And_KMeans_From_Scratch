544b84394e7fc1c638568efc76097ff8425d4d8f
mkanand315 <simrananand99@gmail.com>
2021-03-13 12:44:34 -0500

    updates to citations, comments and variable naming

--- kmeansV3.py
+++ kmeansV3.py
@@ -6,6 +6,7 @@ Citations:
 centroid matrix https://www.youtube.com/watch?v=W4fSRHeafMo
 - Referenced for more insight on stopping criterion 
 for K-Means https://towardsdatascience.com/understanding-k-means-clustering-in-machine-learning-6a6e67336aa1
+- Referenced for more insight on python's enemurate() function: https://www.programiz.com/python-programming/methods/built-in/enumerate
 """
 
 class KMeansModel():
@@ -13,40 +14,40 @@ class KMeansModel():
     """
     Initialization function for the model parameters.
     Based on the assignment description, we will have 150 data points 
-    with 4 features each.
+    with 4 features each with the modified iris dataset: https://archive.ics.uci.edu/ml/index.php.
     """
-    def __init__(self, X, num_of_clusters):
-        self.K = num_of_clusters
-        self.num_of_samples, self.num_of_features = X.shape
-
-    """
-    This function is equivalent to generating random, initial, centroids.
-    Since we are given the initial centroid values, this function simple 
-    returns the specified intial centroids.
-    """
-
+    def __init__(self, X, num_of_k_clusters):
+        self.num_of_samples, self.num_of_features = X.shape             # X.shape returns a tuple (rows, columns)
+        self.K = num_of_k_clusters
+        
     """
     This function goes through each point in the dataset and determines which
     centroid is the closest to that point. Then, it assigns the point to the 
-    closest cluster
+    closest cluster by appending it to the list of that cluster
     """
-    def create_clusters(self, X, centroids):
+    def make_and_assign_to_clusters(self, X, centroids):
+        # Creating a list of list for clusters. One array for each cluster within a single list.
         clusters = [[] for _ in range(self.K)]
 
+        # Looping through each point in training data to assign it to the closest cluster.
         for i, x_in_dataset in enumerate(X):
-            # Determining which centroid is closest to the current point x in the dataset
-            closest_centroid = numpy.argmin(numpy.sqrt(numpy.sum((x_in_dataset - centroids)**2, axis=1)))
-            clusters[closest_centroid].append(i)    
+            # Determining which centroid is closest to the current point x in the dataset using Euclidean Distance.
+            closest_cluster_centroid = numpy.argmin(numpy.sqrt(numpy.sum((x_in_dataset - centroids)**2, axis=1)))
+            # Assigning training point to the closest cluster found in above step.
+            clusters[closest_cluster_centroid].append(i)    
         return clusters
     
     """
-    This functions updates the centroid values by taking the average of the clusters.
+    This functions updates the cluster centroid values by taking the average of each cluster.
     """
     def update_centroids(self, clusters, X):
+        # Initializing array for new set of cluster centroids
         centroids = numpy.zeros((self.K, self.num_of_features))
         
+        # Looping through each cluster to determine the mean of it along each row.
         for i, cluster in enumerate(clusters):
-            updated_centroid = numpy.mean(X[cluster], axis=0)
+            # Computing the mean of the current cluster interation.
+            updated_centroid = numpy.mean(X[cluster], axis=0)           # X[cluster] indexs all dataset points associated with the current cluster
             centroids[i] = updated_centroid
         return centroids
 
@@ -55,10 +56,13 @@ class KMeansModel():
     cluster it belongs to
     """
     def predict_label_of_cluster(self, clusters, X):
+        # Initializing array for label predictions
         pred_labels =  numpy.zeros(self.num_of_samples)
 
+        # Looping through each cluster and points within each cluster to extract label predictions
         for i, cluster in enumerate(clusters):
             for j in cluster:
+                # Assigning prediction label as label = cluster = 1, 2, ... K
                 pred_labels[j] = i
         return pred_labels
 
@@ -67,13 +71,14 @@ class KMeansModel():
     and cluster values
     """
     def fit(self, X):
-        centroids = centroids = numpy.array([[1.03800476, 0.09821729, 1.0469454, 1.58046376],
+        # Initial guess for centroids as given in assignment description
+        centroids = numpy.array([[1.03800476, 0.09821729, 1.0469454, 1.58046376],
                     [0.18982966, -1.97355361, 0.70592084, 0.3957741],
                     [1.2803405, 0.09821729, 0.76275827, 1.44883158]])
 
         stopFlag = 0
         while stopFlag == 0:
-            clusters = self.create_clusters(X, centroids)
+            clusters = self.make_and_assign_to_clusters(X, centroids)
             
             centroids_temp = centroids
             centroids =  self.update_centroids(clusters, X)
